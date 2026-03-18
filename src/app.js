1eae2a5afaead2cf3b26669655f9613c0a02fc7a
Dipesh Namdev <dip.study1@gmail.com>
2026-03-18 12:18:44 +0530

    Production level

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "http://localhost:5173", // Allow only your frontend URL
+  origin: "*", // Allow frontend URL
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true
 }));
--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,19 @@
 import { Pool } from "pg";
 
+// const pool = new Pool({
+//   host: "localhost",
+//   port: 5432,
+//   user: "postgres",
+//   password: "1234",
+//   database: "storage_management_system"
+// })
+
 const pool = new Pool({
-  host: "localhost",
-  port: 5432,
-  user: "postgres",
-  password: "1234",
-  database: "storage_management_system"
-})
+  connectionString: "postgresql://postgres:Roopesh@1905@db.qnztgwhjkwbylwrwcqss.supabase.co:5432/postgres",
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 const connectToDB = async () => {
   console.log(process.env.DB_USER)
